f201f41107584bbf0d98801dd776e8d531584a9d
cynthia <cchelangat9892gmail.com>
2022-11-16 10:32:02 +0300

    Created the home component and styled it

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="container ">
+        <div className="text">DRIVING TOMORROW TODAY</div>
+      </div>
+      <button className="button">SHOP NOW</button>
+    </div>
+  );
+}
+
+export default Home;
