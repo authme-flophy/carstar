bbb9f1f607c576482d8835423dee1226f39fdedc
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-11-16 22:11:30 +0300

    add a navbar and style it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,35 @@
-import React from "react";
-import "./Home.css";
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import './Home.css'
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="heading-container">
-        <h1>DRIVING TOMORROW <br></br> TODAY</h1>
+    <div className='home-container'>
+      <div className='heading-container'>
+        <h1>
+          <TypeAnimation
+            sequence={[
+              'DRIVING',
+              1000,
+              'DRIVING TOMORROW',
+              1000,
+              'DRIVING TOMORROW TODAY',
+              1000,
+            ]}
+            cursor={false}
+            wrapper='div'
+            className='home-animation'
+            easing='ease-in-out'
+            repeat={2}
+          />
+          {/* DRIVING TOMORROW <br></br> TODAY */}
+        </h1>
       </div>
-      <div className="shopnow-btn">
-      <button className="button">SHOP NOW</button>
+      <div className='shopnow-btn'>
+        <button className='button'>SHOP NOW</button>
       </div>
-    
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './navbar.css'
 
 const Navbar = () => {
   return (
     <>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/contact'>Contact</NavLink>
-      <NavLink to='/carsForSale'>CarsForSale</NavLink>
-      {/* <NavLink to='/'>CarsForHire</NavLink> */}
+      <nav className='navbar'>
+        <div className='navbar-brand'>
+          <h2>
+            car<span>star</span>
+          </h2>
+        </div>
+
+        <div className='navbar-nav'>
+          <ul>
+            <li>
+              <NavLink to='/' activeClassName='active'>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to='/login'>Login</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
