0b44e0df67c15e980647458734609e5e3e286db5
cynthia <cchelangat9892gmail.com>
2022-11-17 21:09:25 +0300

    Styled the buttons

--- src/components/CarsForSale.js
+++ src/components/CarsForSale.js
@@ -5,11 +5,11 @@ const CarsForSale = () => {
   return (
     <div className="cars-container">
       <div className="btn-container">
-        <button className="bmw">BMW</button>
-        <button className="audi">AUDI</button>
-        <button className="mercedes">MERCEDES</button>
-        <button className="toyota">TOYOTA</button>
-        <button className="volkswagen">VOLKSWAGEN</button>
+        <button className="car-button">BMW</button>
+        <button className="car-button">AUDI</button>
+        <button className="car-button">MERCEDES</button>
+        <button className="car-button">TOYOTA</button>
+        <button className="car-button">VOLKSWAGEN</button>
       </div>
     </div>
   );
