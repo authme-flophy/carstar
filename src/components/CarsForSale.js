63a15b79ed83c91872ef3b40058615033dd525c8
cynthia <cchelangat9892gmail.com>
2022-11-17 20:12:07 +0300

    Created the buttons in the carsforsale component

--- src/components/CarsForSale.js
+++ src/components/CarsForSale.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./CarsForSale.css";
 
 const CarsForSale = () => {
-  return <div>cars for sale sec</div>
-}
+  return (
+    <div className="cars-container">
+      <div className="btn-container">
+        <button className="bmw">BMW</button>
+        <button className="audi">AUDI</button>
+        <button className="mercedes">MERCEDES</button>
+        <button className="toyota">TOYOTA</button>
+        <button className="volkswagen">VOLKSWAGEN</button>
+      </div>
+    </div>
+  );
+};
 
-export default CarsForSale
+export default CarsForSale;
